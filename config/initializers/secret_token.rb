4bab2466589ae7258b6850e53f115582b7a84849
Oge Nnadi <oge@nnadi.org>
2011-09-04 12:32:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oge::Application.config.secret_token = '6d0b941f86bb1e72a5d4aeb8d80cae20dd832642335e4b7ac3f6c66320088ec67e95c17c2e29f593df8b11a956e9937b32984f2b2896954404e8c390167e0030'
